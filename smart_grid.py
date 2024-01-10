5c59c570ef765a9bbc70517eab760fdeba8b294b
Antonio van Dijck <antonio.vdijck@gmail.com>
2024-01-10 21:05:15 +0100

    Algorithm improvements, added notebook for testing, added data

--- smart_grid.py
+++ smart_grid.py
@@ -7,23 +7,51 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # new dictionary maken
-data_district1 = {}
+def load_battery_data(filename):
 
-csv_file = '/Users/antoniovandijck/Downloads/Huizen&Batterijen/district_1/district-1_batteries.csv'
+    # # new dictionary maken
+    battery_data = {}
 
-with open(csv_file, 'r') as file:
+    with open(filename, 'r') as file:
 
-    csv_reader = csv.reader(file)
+        csv_reader = csv.reader(file)
 
-    next(csv_reader)
-    
-    for row in csv_reader:
-
-        positie = map(int, row[0].split(','))
+        # Overslaan header
+        next(csv_reader)
         
-        capaciteit = row[1]
+        for row in csv_reader:
 
-        data_district1[tuple(positie)] = capaciteit
+            positie = map(int, row[0].split(','))
+            
+            capaciteit = row[1]
 
-print(data_district1)
+            battery_data[tuple(positie)] = capaciteit
+
+    return battery_data
+
+
+def load_house_data(filename):
+
+    house_dict = {}
+
+    with open(filename, 'r') as file:
+            
+            csv_reader = csv.reader(file)
+    
+            # Overslaan header
+            next(csv_reader)
+            
+            for row in csv_reader:
+    
+                x = row[0]
+                y = row[1]
+                maxoutput = row[2]
+    
+                house_dict[tuple((int(x),int(y)))] = float(maxoutput)
+
+    return house_dict
+
+houses_data = ['Huizen&Batterijen/district_1/district-1_houses.csv', 'Huizen&Batterijen/district_2/district-2_houses.csv', 'Huizen&Batterijen/district_3/district-3_houses.csv']
+
+for house in houses_data:
+    print(load_house_data(house))
