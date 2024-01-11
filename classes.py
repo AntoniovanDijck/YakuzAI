9a1d474695086348ecae654dc35be6623b30c574
Antonio van Dijck <antonio.vdijck@gmail.com>
2024-01-11 15:59:29 +0100

    classes update

--- classes.py
+++ classes.py
@@ -1,15 +1,45 @@
-import numpy as np
-import csv
 #Main classes for github
 # Antonio, Mec, Vincent
 # YakuzAI
 
-class Object():
-    def __init__(self, pos_x, pos_y, color):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.color = "black"
+import numpy as np
+import csv
 
-class House(Object):
-    def __init__(self, pos_x, pos_y, color):
-        super.__init__(pos_x, pos_y, color)
+from smart_grid import load_battery_data, load_house_data, show_district
+
+class Battery():
+    def __init__(self, x, y, capacity):
+
+        # de x en y
+        self.x = x
+        self.y = y
+
+        # de capaciteit en de maximale output
+        self.capacity = capacity
+
+class House():
+    def __init__(self, x, y, maxoutput):
+
+        # de x
+        self.x = x
+
+        # de y
+        self.y = y
+
+        # de output
+        self.maxoutput = maxoutput
+
+
+class Cable():
+    def __init__(self, house, battery):
+
+        # de batterij
+        self.battery = battery
+
+        # het huis
+        self.house = house
+
+class District():
+    def __init__(self, houses, batteries):
+
+        show_district(houses, batteries)
