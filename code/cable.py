ac8f5ca4777452ec0833abc551c12cabb4c2ed83
Mec <mrgreen@mm.local>
2024-01-13 13:21:04 +0100

    Deactivated cable module

--- code/cable.py
+++ code/cable.py
@@ -1,20 +1,20 @@
-import numpy as np
-import csv
+# import numpy as np
+# import csv
 
-from code.smart_grid import load_battery_data, load_house_data, show_district
-from code.house import House
-from code.battery import Battery
+# from code.smart_grid import load_battery_data, load_house_data, show_district
+# from code.house import House
+# from code.battery import Battery
 
 
-class Cable():
-    def __init__(self, house, battery):
+# class Cable():
+#     def __init__(self, house, battery):
 
-        # de batterij
-        self.battery = battery
+#         # de batterij
+#         self.battery = battery
 
-        # het huis
-        self.house = house
-        self.length = self.calculate_length()
+#         # het huis
+#         self.house = house
+#         self.length = self.calculate_length()
 
-    def calculate_length(self):
-        return abs(self.house.x - self.battery.x)+(self.house.y - self.battery.y)
+#     def calculate_length(self):
+#         return abs(self.house.x - self.battery.x)+(self.house.y - self.battery.y)
