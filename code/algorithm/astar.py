8c35d3fd354e3e34052ed7996a6700943624eec2
Antonio van Dijck <antonio.vdijck@gmail.com>
2024-01-23 17:09:07 +0100

    comments

--- code/algorithm/astar.py
+++ code/algorithm/astar.py
@@ -1,61 +1,47 @@
-# Smart_grid.py
+# astar.py
 # Antonio, Mec, Vincent
 # YakuzAI
 
+##TODO: THIS IS STILL GREEDY 2.0, NOT A* YET
 class A_star:
     def __init__(self, district):
         self.district = district
 
     def distance(self, house, battery):
-        ''' Function that calculates the Manhattan distance between a house and a battery'''
+        ''' Function that calculates the distance between a house and a battery'''
+
+        # Calculate distance between house and battery using the Manhattan distance
         return abs(battery.x - house.x) + abs(battery.y - house.y)
 
-    def a_star_search(self, house, battery):
-        batteries_sorted = [(self.distance(house, battery), house)]
-        vorige_node = {}
-        g_score = {node: float("inf") for node in self.district.batteries}
-        g_score[house] = 0
+    def connect_houses_to_batteries(self):
+        ''' Function that connects houses to batteries'''
 
-        while batteries_sorted:
-            batteries_sorted.sort(key=lambda x: x[0])
-            current = batteries_sorted.pop(0)[1]
-
-            if current == battery:
-                break
-
-            for next in current.connections:
-                new_g_score = g_score[current] + self.distance(current, next)
-
-                if new_g_score < g_score[next]:
-                    vorige_node[next] = current
-                    g_score[next] = new_g_score
-
-                    # Check if next is already in batteries_sorted
-                    for i, (existing_f_score, existing_node) in enumerate(batteries_sorted):
-                        if existing_node == next:
-                            if new_g_score + self.distance(next, battery) < existing_f_score:
-                                batteries_sorted[i] = (new_g_score + self.distance(next, battery), next)
-                            break
-                    else:
-                        batteries_sorted.append((new_g_score + self.distance(next, battery), next))
-
-        return vorige_node
-    
-    def connect_houses_to_batteries2(self):
+        # loop through houses in the district
         for house in self.district.houses:
-            # maak een lijst met de batterijen en hun afstand t.o.v huis
-            batteries_sorted = [(self.distance(house, battery), battery.x, battery) for battery in self.district.batteries]
- 
-            # Sort batteries weer op afstand
-            batteries_sorted.sort()
 
-            # Loop door de batterijen heen en kijk of het huis verbonden kan worden
-            for h, d, battery in batteries_sorted:
+            # add battery and distance to list using the distance function
+            house.battery_distances = [(battery, self.distance(house, battery)) for battery in self.district.batteries]
+
+            # Sort by distance, second culumn [1] using lamda function. 
+            house.battery_distances.sort(key=lambda x: x[1])
+
+        # Connect houses to batteries
+        for house in self.district.houses:
+            
+            # Try to connect to the closest battery first
+            for battery, y in house.battery_distances:
+
+                # If the battery can connect to the house, connect them
                 if battery.can_connect(house):
+
+                    # Place cables
                     self.place_cables(house, battery)
                     battery.connect_house(house)
+
+                    # Stop looking for batteries if connected
                     break
             else:
+                # if house could not be connected to any battery print error
                 print(f"House at ({house.x}, {house.y}) could not be connected to any battery.")
 
     def place_cables(self, house, battery):
