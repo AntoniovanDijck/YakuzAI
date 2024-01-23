69a0054c48220e62782cfd10912ed63daab5ebef
Antonio van Dijck <antonio.vdijck@gmail.com>
2024-01-23 14:01:00 +0100

    added helper get 5 total length

---
+++ code/helpers/get_5_total_length.py
@@ -0,0 +1,15 @@
+# Smart_grid.py
+# Antonio, Mec, Vincent
+# YakuzAI
+
+
+def get_5_total_length(batteries):
+    total_cable_length = 0
+    
+    battery_list = list(batteries.values())[:5]
+    for battery in battery_list:
+        #get first 50 batteries
+        for cable in battery.cables:
+            total_cable_length += cable.length
+    
+    return total_cable_length
