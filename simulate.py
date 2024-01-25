7698b85767fd317cf8e0976e87f9cc1d5913ce78
Vincent D'Andolfi <vincent.dandolfi@gmail.com>
2024-01-25 16:30:46 +0100

    nearest object is performing better then nearest battery, draw cables doest not work on it

--- simulate.py
+++ simulate.py
@@ -45,13 +45,13 @@ class Simulate_algorithm:
 district1_houses = 'data/Huizen&Batterijen/district_1/district-1_houses.csv'
 district1_batteries = 'data/Huizen&Batterijen/district_1/district-1_batteries.csv'
 
-sim_rand = Simulate_algorithm(RandomAlgorithm, 1000).simulate()
+sim_rand = Simulate_algorithm(RandomAlgorithm, 10).simulate()
 
 #run the algorithm for nearest battery and plot
-sim_near_batt = Simulate_algorithm(nearest_battery, 1000).simulate()
+sim_near_batt = Simulate_algorithm(nearest_battery, 10).simulate()
 
 #run the algorithm for nearest object and plot
-sim_near_obj = Simulate_algorithm(nearest_object, 1000).simulate()
+sim_near_obj = Simulate_algorithm(nearest_object, 10).simulate()
 
 #plot the sim in a simple bar chart with bins of 100, the amount of times the total costs are in a bin is the frequency
 plt.hist(sim_rand, bins=50)
