4c249385cb90ce3df839b91d379704774fd28dfe
Vincent D'Andolfi <vincent.dandolfi@gmail.com>
2024-01-27 18:24:13 +0100

    Now, the lowest found cost per algoritm saves the house in the input order in a csv that we can visualize

--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ from code.algorithm.nearest_object_x import nearest_object_x
 from code.algorithm.nearest_object_y import nearest_object_y
 from code.algorithm.nearest_object_rand import nearest_object_rand
 import matplotlib.pyplot as plt
+import csv
 import numpy as np
 
 class simulate_algorithm:
@@ -47,7 +48,8 @@ class simulate_algorithm:
             if not is_initial_cost_set or total_costs < self.lowest_costs:
                 self.lowest_costs = total_costs
                 self.lowest_district = district
-                self.lowest_cost_house_order = [house for house in district.houses]
+                self.lowest_cost_house_order = [(house.x, house.y, house.maxoutput) for house in district.houses]
+
                 is_initial_cost_set = True
 
         print(f"Lowest costs: {self.lowest_costs} for algorithm {self.algorithm.__name__}")
@@ -60,25 +62,19 @@ class simulate_algorithm:
         return self.lowest_cost_house_order
     
 
+    def save_lowest_cost_house_order_to_csv(self, file_name):
+        """Saves the house order for the lowest cost to a CSV file."""
+        
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['x', 'y', 'maxoutput'])  # Header
 
-    def save_results_to_file(self, file_name_prefix, save_directory):
-        """Save the results to a file in the specified directory."""
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
-
-        file_path = os.path.join(save_directory, f"{file_name_prefix}_results.json")
-        results = {
-            'lowest_cost': self.lowest_costs,
-            'house_order': [(house.x, house.y) for house in self.lowest_cost_house_order]
-        }
-        with open(file_path, 'w') as file:
-            json.dump(results, file, indent=4)
+            for house in self.lowest_cost_house_order:
+                writer.writerow(house)
 
 
 
 def experiment(houses_file, batteries_file, iterations=100):
-
-
     # Create instances for each algorithm
     sim_rand_instance = simulate_algorithm(RandomAlgorithm, iterations, houses_file, batteries_file)
     sim_battery_instance = simulate_algorithm(nearest_battery, iterations, houses_file, batteries_file)
@@ -86,55 +82,54 @@ def experiment(houses_file, batteries_file, iterations=100):
     sim_object_y_instance = simulate_algorithm(nearest_object_y, iterations, houses_file, batteries_file)
     sim_obj_rand_instance = simulate_algorithm(nearest_object_rand, iterations, houses_file, batteries_file)
 
-    # Run simulations
-    print("1/5")
+    # Define the directory to save the results
+    save_directory = "simulation_results"
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
+    # Run simulations and save results for each algorithm
+    print("1/5: Running RandomAlgorithm Simulation")
     sim_rand = sim_rand_instance.simulate()
-    print("2/5")
+    csv_filename_rand = os.path.join(save_directory, 'random_algorithm_lowest_cost_order.csv')
+    sim_rand_instance.save_lowest_cost_house_order_to_csv(csv_filename_rand)
+
+    print("2/5: Running nearest_battery Simulation")
     sim_battery = sim_battery_instance.simulate()
-    print("3/5")
+    csv_filename_battery = os.path.join(save_directory, 'nearest_battery_lowest_cost_order.csv')
+    sim_battery_instance.save_lowest_cost_house_order_to_csv(csv_filename_battery)
+
+    # Repeat for other algorithms
+    print("3/5: Running nearest_object_x Simulation")
     sim_object_x = sim_object_x_instance.simulate()
-    print("4/5")
+    csv_filename_object_x = os.path.join(save_directory, 'nearest_object_x_lowest_cost_order.csv')
+    sim_object_x_instance.save_lowest_cost_house_order_to_csv(csv_filename_object_x)
+
+    print("4/5: Running nearest_object_y Simulation")
     sim_object_y = sim_object_y_instance.simulate()
-    print("5/5")
+    csv_filename_object_y = os.path.join(save_directory, 'nearest_object_y_lowest_cost_order.csv')
+    sim_object_y_instance.save_lowest_cost_house_order_to_csv(csv_filename_object_y)
+
+    print("5/5: Running nearest_object_rand Simulation")
     sim_obj_rand = sim_obj_rand_instance.simulate()
+    csv_filename_obj_rand = os.path.join(save_directory, 'nearest_object_rand_lowest_cost_order.csv')
+    sim_obj_rand_instance.save_lowest_cost_house_order_to_csv(csv_filename_obj_rand)
 
-    # Determine the common range for all histograms
-    all_values = sim_rand | sim_battery | sim_object_x | sim_obj_rand
+    # Plotting and saving the histogram
+    all_values = sim_rand | sim_battery | sim_object_x | sim_object_y | sim_obj_rand
     min_value, max_value = min(all_values), max(all_values)
-
-    # Standardize bin edges
     bins = np.linspace(min_value, max_value, int(np.sqrt(iterations)))
 
-    # Plot the histograms
     plt.hist(sim_rand, bins=bins, alpha=0.5)
     plt.hist(sim_battery, bins=bins, alpha=0.5)
     plt.hist(sim_object_x, bins=bins, alpha=0.5)
     plt.hist(sim_object_y, bins=bins, alpha=0.5)
     plt.hist(sim_obj_rand, bins=bins, alpha=0.5)
-
-    # Title and labels
     plt.title(f"Comparison of Algorithms with {iterations} iterations")
     plt.xlabel("Total Costs")
     plt.ylabel("Frequency")
-
-    # Legend
     plt.legend(["Random", "Nearest Battery", "Nearest Object X", "Nearest Object Y", "Nearest Object Rand"])
-
-    # plt.show()
-
-    # Define the directory to save the results
-    save_directory = "simulation_results"
-
-    # Save results for each algorithm
-    save_directory = "simulation_results"
-    sim_rand_instance.save_results_to_file("random_algorithm", save_directory)
-    sim_battery_instance.save_results_to_file("nearest_battery", save_directory)
-    sim_object_x_instance.save_results_to_file("nearest_object_x", save_directory)
-    sim_object_y_instance.save_results_to_file("nearest_object_y", save_directory)
-    sim_obj_rand_instance.save_results_to_file("nearest_object_rand", save_directory)
-
-    # Save the figure
-    plt.savefig(os.path.join(save_directory, "simulation_histogram.png"))   
+    plt.savefig(os.path.join(save_directory, "simulation_histogram.png"))
+ 
 
 
 # run the algorithm for district 1
