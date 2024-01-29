dded640d41ae9629ee51b27b2aa92f3c91d44bef
mec.glandorff@student.uva.nl <mec.glandorff@student.uva.nl>
2024-01-29 11:28:50 +0100

    hillclimber updates

---
+++ code/algorithm/Hillclimber.py
@@ -0,0 +1,20 @@
+
+from nearest_object_y import nearest_object_y
+
+
+class Hillclimber:
+    def __init__(self, district):
+        self.district = district
+        self.near_obj_y = nearest_object_y(district)
+        self.best_solution = None
+        self.best_cost = float("inf")
+
+    def initial_solution(self):
+        """Generate start population based upon the nearest obj y algorithm"""
+        self.near_obj_y.connect_houses_to_batteries()
+        self.best_solution = self.current_solution()
+        self.best_cost = self.evaluate_solution(self.best_solution)
+
+    def current_solution(self):
+        """Get the current solution from the algoritmh"""
+        pass
