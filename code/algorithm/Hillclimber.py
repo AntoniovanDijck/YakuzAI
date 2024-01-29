9d93c40776552949e0981d4c5b3b6ad6afd51dc1
mec.glandorff@student.uva.nl <mec.glandorff@student.uva.nl>
2024-01-29 11:52:08 +0100

    hillclimber updates

--- code/algorithm/Hillclimber.py
+++ code/algorithm/Hillclimber.py
@@ -33,5 +33,28 @@ class Hillclimber:
         #implement later
         pass
 
-    def hillclimber(self):
+    def hillclimber(self, iterations=100):
+        """Here the hillclimb optimization is performed for a given iterations"""
         
+        self.current_solution
+
+        for _ in range(iterations):
+            self.solution_change()
+            current_solution = self.current_solution()
+            current_cost = self.evaluate_solution(current_solution)
+
+            if current_cost < self.best_cost:
+                self.best_cost = current_solution
+                self.best_cost = current_cost
+            
+            else:
+                self.undo_change()
+
+    def undo_change(self):
+        """undo the last change if it increased the cost"""
+
+        #implement later
+        pass
+
+
+
